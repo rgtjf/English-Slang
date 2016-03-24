23ae3f909ed330be7537282ee08f1ad9dbfe6297
rgtjf <rgtjf@users.noreply.github.com>
2016-03-24 21:42:46 +0800

    Update function.js
    remove comments

--- js/function.js
+++ js/function.js
@@ -8,61 +8,9 @@
 $(document).ready(function() {
 	
 	var slang = '';
-	/*
-        $(".Checked").click(function(){
-
-        	//not change icon
-        	//! $(this).prop("checked");
-
-		//console.log($.session.get('chineseName'));
-		//$.session.remove('chineseName');
-		//$.session.remove('englishName');
-		
-		var div = $(this).closest(".singleitem"); //children("span").toggleClass("black");
-	        slang = div.children("span").text().trim();
-		
-		if ($.session.get('chineseName') == undefined || $.session.get('englishName') == undefined) {
-			//console.log('x');
-			$('#myModal').modal();
-		}
-		else {
-			var chineseName = $.session.get('chineseName');
-			var englishName = $.session.get('englishName');
-			$.ajax({
-	 	               type:"POST",
-	        	    	url: "update.php",
-	              		data: "type=2&chineseName="+chineseName+"&englishName="+englishName+"&slang="+slang,
-	                	success:function(msg){
-	                    		// alert(msg);
-					// location.reload(true);
-					// TODO: p->hidden(); class->.UnChecked(); id->.UnChecked();							
-	               			// TODO: change according to return state && stop all operation;
-					// alert(msg);
-					//
-                			
-					//! div.children("input").prop("checked");
-                			
-                			div.children("span").toggleClass("black grey");
-                			div.children("input").toggleClass("Checked UnChecked");
-					div.children("p").toggleClass("p_show p_hidden");
-				}
-	        	});
-		}
-        });
-	*/
 
  	$(".UnChecked, .Checked").click(function(){
-
- 		//not change icon
-        	//! $(this).prop("checked");
-
-		
-		//$.session.remove('chineseName');
-		//$.session.remove('englishName');
-		//console.log($.session.get('chineseName'));
-		//$.session.set('chineseName', 'AN WeiJie');
-		//$.session.set('englishName', 'Fssqawj');
-		
+ 		
 		var div = $(this).closest(".singleitem");
 	        slang = div.children("span").text().trim();
 		
@@ -74,22 +22,13 @@ $(document).ready(function() {
 			var englishName = $.session.get('englishName');
 	       	
 			var now = div.children("input").attr('class');	
-			//console.log(slang);
-		      	//$('.Checked').prop("disabled", true);
-			//$('.UnChecked').prop('disabled', true);
+			
 			$.ajax({
 	 	               	type:"POST",
 	        	    	url: "update.php",
 	              		data: "type="+now+"&chineseName="+chineseName+"&englishName="+englishName+"&slang="+slang,
 	                	success:function(msg){
-	                    		// alert(msg);
-					// location.reload(true);
-					// TODO: p->show(); class->.Checked(); id->.UnChecked()						
-	               			// TODO: change according to return state && stop all operation;
-					
-					//$('.Check').prop("disable", false);
-					//$('.UnCheck').prop("disable", false);
-					console.log(msg);
+	                 
 					if (msg == "full") {
 						alertify.log('You have already choosed 4 slang!...');
 						div.children("input").removeAttr("checked");
@@ -116,8 +55,6 @@ $(document).ready(function() {
 		else {
 			$.session.set('chineseName', chineseName);
 			$.session.set('englishName', englishName);
-			//$.post("backend.php", {"chineseName":chineseName, "englishName":englishName});
-			//location.reload(true);
 			$.ajax({
 	 	              	type:"POST",
 	        	    	url: "update.php",
@@ -128,7 +65,6 @@ $(document).ready(function() {
 					location.reload(true);
 				}
 	        	});	
-			//location.reload(true);
 		}
 	});
 });
